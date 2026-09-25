95b5de2be9f4614b608d6e7ec3e16e5f88208c7b
yeyione <jennifer.estradaavendano@gmail.com>
2026-09-25 08:40:59 -0700

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-root',
+  styleUrl: './app.css',
+  templateUrl: './app.html',
+})
+export class App {
+  protected readonly title = signal('catalogo-productos');
+}
